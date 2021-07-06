1faeb229eb9f9d3eef0ccc18fde0f6068acbf699
Sherwat <72201712+Sherwat@users.noreply.github.com>
2021-07-06 17:06:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function redirect_bedroom()
+{
+    window.location = "bedroom.html"
+}
+
+function redirect_tv_room()
+{
+    window.location = "tv_room.html"
+}
+
+function redirect_drawing_room()
+{
+    window.location = "drawing_room.html"
+}
+
+function redirect_dining_room()
+{
+    window.location = "dining_room.html"
+}
+
+function redirect_bathroom()
+{
+    window.location = "bathroom.html"
+}
+
+function redirect_kitchen()
+{
+    window.location = "kitchen.html"
+}
